52aef1b538f941038bdf0b13c1ff5694b3178bc9
ludwig-pro <hello@ludwigvantours.dev>
2021-08-05 11:58:27 +0200

    fix: type

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,13 @@
 declare module 'react-native-esp-idf-provisioning' {
-  export interface BLEDeveice {
+  export interface BLEDevice {
     name: string;
-    id: string;
+    address: string;
   }
-  export function getBleDevices(): Promise<BLEDevice>;
+  export function getBleDevices(prefix: string): Promise<BLEDevice>;
+  export interface ConnectBleDevice {
+    deviceAddress: string;
+    security: 1 | 0;
+    deviceProofOfPossession: string;
+  }
+  export function connectBleDevice(arg: ConnectBleDevice): Promise<any>;
 }
